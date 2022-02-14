894127d74a1b6eb8a4f7d0d234a97d6792be7b80
mahajanakshat07 <57159963+mahajanakshat07@users.noreply.github.com>
2022-02-14 13:02:58 -0600

    Add files via upload

--- main.tf
+++ main.tf
@@ -20,51 +20,18 @@ resource "aws_glue_catalog_database" "data" {
 
 
 
-
-resource "aws_glue_job" "version1" {
-  name     = "version1"
-  role_arn = "arn:aws:iam::186972323852:role/service-role/AWSGlueServiceRole-Etl"
-glue_version = "3.0"
-  worker_type = "G.1X"
-  number_of_workers = "10"
-
+module "glue_crawler" {
+  source = "./modules/glue_crawler"
   
+}
+module "glue_job" {
+  source = "./modules/glue_job"
   
-
-  
-
-  command {
-    script_location = "s3://fifadataforetl/code.py"
-    python_version = "3"
-  }
-  
-
+}
+module "trigger" {
+  source = "./modules/glue_trigger"
+  job_name = module.glue_job.name_of_job
 
 }
 
 
-
-    resource "aws_glue_crawler" "democrawler" {
-  database_name = aws_glue_catalog_database.data.name
-  name          = "democrawler"
-  role          =  "arn:aws:iam::186972323852:role/service-role/AWSGlueServiceRole-Etl"
-  schedule = "cron(*/5 * * * ? *)"
-  type     = "SCHEDULED"
-
-
-  
-  s3_target {
-    path = "s3://fifadataforetl/terraform/data.csv"
-  }
-
-}
-
-  resource "aws_glue_trigger" "trigger2" {
-  name     = "example"
-  schedule = "cron(*/5 * * * ? *)"
-  type     = "SCHEDULED"
-
-  actions {
-    job_name = aws_glue_job.version1.name
-  }
-}
